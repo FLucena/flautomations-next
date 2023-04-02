227bc860d8959c103df79b8b81aa1ae346de02f3
Francisco Lucena <franciscolucena90@gmail.com>
2023-04-02 04:12:39 -0300

    update posts

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -1,8 +1,23 @@
 import { google } from 'googleapis';
 import NavBar from '../../components/Navbar';
 import styles from './[id].module.css';
+import LRUCache from 'lru-cache';
+
+const cache = new LRUCache({
+  max: 100, // maximum number of items to store in cache
+  maxAge: 60 * 1000, // cache for 1 minute
+});
 
 export async function getServerSideProps({ query }) {
+  const { id } = query;
+  const range = `Sheet1!A${id}:E${id}`;
+
+  // Check cache for existing response
+  const cachedResponse = cache.get(range);
+  if (cachedResponse) {
+    return { props: cachedResponse };
+  }
+
   // Auth
   const client = new google.auth.JWT(
     process.env.client_email,
@@ -13,10 +28,7 @@ export async function getServerSideProps({ query }) {
 
   const sheets = google.sheets({ version: 'v4', auth: client });
 
-  // Query
-  const { id } = query;
-  const range = `Sheet1!A${id}:E${id}`;
-  
+  // Call API
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
     range,
@@ -25,48 +37,49 @@ export async function getServerSideProps({ query }) {
   // Check if response.data.values exists and is not empty
   if (response.data.values && response.data.values.length > 0) {
     const [title, content, video, image, defaultImg] = response.data.values[0];
-    return {
-      props: {
-        title,
-        content,
-        video,
-        image,
-        defaultImg
-      },
+    console.log(response);
+
+    // Store response in cache
+    const props = {
+      title,
+      content,
+      video,
+      image,
+      defaultImg,
     };
+    cache.set(range, props);
+
+    return { props };
   } else {
-    return {
-      notFound: true,
-    };
+    return { notFound: true };
   }
 }
 
 export default function Post({ title, content, video, image, defaultImg }) {
-    return (
-        <>
-            <NavBar/>
-            <div className={styles.post}>
-                
-                <h1 className={styles.title}>{title}</h1>
-                {image ? (
-                    <div className={styles.imgWrapper}>
-                        <img src={image} alt="post image" />
-                    </div>
-                ) : (
-                    <div className={styles.imgWrapper}>
-                        <img src={defaultImg} alt="no image"/>
-                        <p className={styles.message}>No image available</p>
-                    </div>
-                )}
-                <div className={styles.content}>{content}</div>
-                {video ? (
-                    <div className={styles.videoWrapper}>
-                        <iframe className={styles.video} src={video} allow="autoplay" />
-                    </div>
-                ) : (
-                    <p className={styles.message}>No video available</p>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <div className={styles.post}>
+        <h1 className={styles.title}>{title}</h1>
+        {image ? (
+          <div className={styles.imgWrapper}>
+            <img src={image} alt="post image" />
+          </div>
+        ) : (
+          <div className={styles.imgWrapper}>
+            <img src={defaultImg} alt="no image" />
+            <p className={styles.message}>No image available</p>
+          </div>
+        )}
+        <div className={styles.content}>{content}</div>
+        {video ? (
+          <div className={styles.videoWrapper}>
+            <iframe className={styles.video} src={video} allow="autoplay" />
+          </div>
+        ) : (
+          <p className={styles.message}>No video available</p>
+        )}
+      </div>
+    </>
+  );
 }
