776f4f15431d1df724f160b26bf7a112609969bf
Francisco Lucena <franciscolucena90@gmail.com>
2023-04-09 06:11:42 -0300

    working api v3

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,6 @@ export default function Post(props) {
       <div className={styles.post}>
         <h1 className={styles.title}>{props[0]}</h1>
         <p>{props[1]}</p>
-        <img src={props[3] || props.defaultImg} alt={props[0]} />
       </div>
     </>
   );
