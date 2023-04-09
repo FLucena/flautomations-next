989cfb3a64e35c5d10dd4522beb46b4421826414
Francisco Lucena <franciscolucena90@gmail.com>
2023-04-09 06:14:34 -0300

    working api v4

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,8 @@
-import NavBar from './Navbar';
-import styles from './Post.module.css';
-
 export default function Post(props) {
   return (
     <>
-      <NavBar />
-      <div className={styles.post}>
-        <h1 className={styles.title}>{props[0]}</h1>
+      <div>
+        <h1>{props[0]}</h1>
         <p>{props[1]}</p>
       </div>
     </>
