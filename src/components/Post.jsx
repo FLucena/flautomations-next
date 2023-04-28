5808cd1b3c76bd0bfdbd0fcc5216972996c43aa2
Francisco Lucena <franciscolucena90@gmail.com>
2023-04-28 17:15:03 -0300

    added post style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,48 +1,88 @@
 import Image from 'next/image';
 import Link from 'next/link';
-export default function Post(props) {
 
+const styles = {
+  wrapper: {
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+  },
+  imageWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '20px',
+  },
+  imageWrapperP: {
+    margin: '0',
+    padding: '10px',
+  },
+  videoWrapper: {
+    width: '100%',
+    height: '400px',
+    border: 'none',
+  },
+  message: {
+    color: 'red',
+    fontWeight: 'bold',
+  },
+};
+
+export default function Post(props) {
   const keys = Object.keys(props);
-  const values = keys.map(key => decodeURIComponent(props[key]));
-  const encodedValues = values.map(val => encodeURI(val));
+  const values = keys.map((key) => decodeURIComponent(props[key]));
+  const encodedValues = values.map((val) => encodeURI(val));
   const { id } = props;
-  const { postClassName, titleClassName, imgWrapperClassName, messageClassName  } = encodedValues;
   const nextPageId = parseInt(id) + 1;
-  const nextPageUrl = `/api/${nextPageId}`;
+  const nextPageUrl = `/api/posts/${nextPageId}`;
   const previousPageId = parseInt(id) - 1;
-  const previousPageUrl = `/api/${previousPageId}`;
-  
+  const previousPageUrl = `/api/posts/${previousPageId}`;
+
   return (
-    <div className={postClassName}>
-      <h1 className={titleClassName}>{decodeURI(encodedValues[0])}</h1>
+    <div style={styles.wrapper}>
+      <h1>{decodeURI(encodedValues[0])}</h1>
       <p>{decodeURI(encodedValues[1])}</p>
-      {encodedValues[3] !== "" ? (
-          <div className={imgWrapperClassName}>
-            <Image className="" src={decodeURI(encodedValues[3])} alt="Post image" width={500} height={500} />
-          </div>
-        ) : (
-          <div className={imgWrapperClassName}>
-            <Image className="" src={decodeURI(encodedValues[4])} alt="No image" width={500} height={500} />
-            <p className={messageClassName}>No image available</p>
-          </div>
-        )}
-      <br></br>
-      {encodedValues[2] !== "" ? (
-        <div className="">
-          <iframe className="" src={encodedValues[2]} allow="autoplay" />
+      {encodedValues[3] !== '' ? (
+        <div style={styles.imageWrapper}>
+          <Image
+            src={decodeURI(encodedValues[3])}
+            alt="Post image"
+            width={500}
+            height={500}
+          />
         </div>
       ) : (
-        <p className={messageClassName}>No video available</p>
+        <div style={styles.imageWrapper}>
+          <Image
+            src={decodeURI(encodedValues[4])}
+            alt="No image"
+            width={500}
+            height={500}
+          />
+          <p style={styles.imageWrapperP}>No image available</p>
+        </div>
       )}
-      <br></br>
+      <br />
+      {encodedValues[2] !== '' ? (
+        <div>
+          <iframe
+            style={styles.videoWrapper}
+            src={encodedValues[2]}
+            allow="autoplay"
+          />
+        </div>
+      ) : (
+        <p style={styles.message}>No video available</p>
+      )}
+      <br />
       <div>
-      <Link href={previousPageUrl}>
-          Previous page
-      </Link>
-      &nbsp; &nbsp; &nbsp; &nbsp;
-      <Link href={nextPageUrl}>
-        Next page
-      </Link>
+        {previousPageId !== 0 ? (
+          <>
+            <Link href={previousPageUrl}>Previous page</Link>
+          </>
+        ) : null}
+        &nbsp; &nbsp; &nbsp; &nbsp;
+        <Link href={nextPageUrl}>Next page</Link>
       </div>
     </div>
   );
--- src/pages/api/posts/[id].js
+++ src/pages/api/posts/[id].js
@@ -1,90 +1,38 @@
-import { google } from 'googleapis';
-import NavBar from '../../../components/Navbar';
-import LRUCache from 'lru-cache';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { renderToString } from 'react-dom/server';
+import Post from '../../../components/Post';
 
-const cache = new LRUCache({
-  max: 100, // maximum number of items to store in cache
-  maxAge: 60 * 1000, // cache for 1 minute
-});
+const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
 
-export async function getServerSideProps({ query }) {
-  const { id } = query;
-  const range = `Sheet1!A${id}:E${id}`;
-
-  // Check cache for existing response
-  const cachedResponse = cache.get(range);
-  if (cachedResponse) {
-    return { props: cachedResponse };
+export default async function handler(req, res) {
+  const { id } = req.query;
+  const range = `A${id}:F${id}`;
+  if (req.method !== 'GET') {
+    res.status(405).end();
+    return;
   }
-
-  const b64_publicKey = Buffer.from(process.env.GOOGLE_PRIVATE_KEY).toString('base64');
-  const pemKey = `
-  -----BEGIN PUBLIC KEY-----
-  ${b64_publicKey}
-  -----END PUBLIC KEY-----
-  `;
   
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+    });
 
-  // Auth
-  const client = new google.auth.JWT(
-    process.env.GOOGLE_CLIENT_EMAIL,
-    null,
-    pemKey,
-    ['https://www.googleapis.com/auth/spreadsheets']
-  );
-
-  const sheets = google.sheets({ version: 'v4', auth: client });
-  // Call API
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SHEET_ID,
-    range,
-  });
-
-  // Check if response.data.values exists and is not empty
-  if (response.data.values && response.data.values.length > 0) {
-    const [title, content, video, image, defaultImg] = response.data.values[0];
-
-    // Store response in cache
-    const props = {
-      title,
-      content,
-      video,
-      image,
-      defaultImg,
-    };
-    cache.set(range, props);
-
-    return { props };
-  } else {
-    return { notFound: true };
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    await sheet.loadCells(range);
+    const rowValues = [];
+    let val = "";
+    for (let col = 0; col < 6; col++) {
+      const cell = sheet.getCell(id - 1, col);
+      cell.value == null ? val=  cell.value = "" : val = cell.value
+      rowValues.push(encodeURIComponent(val));
+    }
+    const postHtml = renderToString(<Post {...rowValues} id={id} imageSRC={rowValues[3]} />);
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.status(200).send(postHtml);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
-}
-
-export default function Post(props) {
-  return (
-    <>
-      <NavBar />
-      <div className={styles.post}>
-        <h1 className={styles.title}>{ props.title }</h1>
-        {props.image ? (
-          <div className={styles.imgWrapper}>
-            <img src={props.image} alt="post image" />
-          </div>
-        ) : (
-          <div className={styles.imgWrapper}>
-            <img src={props.defaultImg} alt="no image" />
-            <p className={styles.message}>No image available</p>
-          </div>
-        )}
-        <div className={styles.content}>{props.content}</div>
-        {props.video ? (
-          <div className={styles.videoWrapper}>
-            <iframe className={styles.video} src={props.video} allow="autoplay" />
-          </div>
-        ) : (
-          <p className={styles.message}>No video available</p>
-        )}
-      </div>
-    </>
-  );
 }
