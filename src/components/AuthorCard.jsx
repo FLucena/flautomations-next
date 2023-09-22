f8a2f0fcc5a9940d29221a780a6765108e25e3d9
Francisco Lucena <franciscolucena90@gmail.com>
2023-09-22 02:35:33 -0300

    updated post v1

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -1,78 +1,43 @@
-import styled from 'styled-components';
-import Image from 'next/image';
-
-const AuthorCardContainer = styled.div`
-  max-width: 700px;
-  margin: 20px;
-  display: flex;
-  align-items: center;
-  padding: 2rem;
-  border-radius: 12px;
-  background-color: #fff;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-const Avatar = styled.div`
-  margin-right: 2rem;
-  border-radius: 50% 0 50% 50%;
-  overflow: hidden;
-  width: 50vw;
-  height: 55vw;
-  max-width: 100px;
-  max-height: 111px;
-
-  @media (max-width: 768px) {
-    margin-right: 0;
-    margin-bottom: 1rem;
-    border-radius: 50%;
-  }
-`;
-
-const Bio = styled.p`
-  font-size: 1.2rem;
-  line-height: 1.5;
-  color: #555;
-
-  @media (max-width: 768px) {
-    font-size: 0.8rem;
-  }
-`;
-
-const Name = styled.div`
-  flex: 50%;
-  font-size: 1rem;
-  @media (max-width: 768px) {
-    font-size: 0.8rem;
-  }
-`;
-
-const Details = styled.div`
-  @media (max-width: 768px) {
-    text-align: center;
-  }
-`;
+import {
+  Box,
+  Text,
+  Flex,
+  Avatar,
+} from '@chakra-ui/react';
+import NextImage from 'next/image';
 
 export default function AuthorCard(props) {
   const { authorName, authorImg, authorDescription } = props;
 
   return (
-    <AuthorCardContainer>
-      <Avatar>
-        <Image src={authorImg} alt="Author Avatar" width={100} height={110} />
+    <Box
+      maxW="700px"
+      margin="20px"
+      display="flex"
+      alignItems="center"
+      p="2rem"
+      borderRadius="12px"
+      backgroundColor="white"
+      boxShadow="0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)"
+    >
+      <Avatar
+        mr="2rem"
+        borderRadius="50%"
+        overflow="hidden"
+        w="50vw"
+        h="55vw"
+        maxW="100px"
+        maxH="111px"
+      >
+        <NextImage src={authorImg} alt="Author Avatar" width={100} height={110} />
       </Avatar>
-      <Details>
-        <Name>
-          <h1>{authorName}</h1>
-        </Name>
-        <div>
-          <Bio>{authorDescription}</Bio>
-        </div>
-      </Details>
-    </AuthorCardContainer>
+      <Flex direction="column" flex="50%">
+        <Text fontSize="1.8rem" mb="1rem">
+          {authorName}</Text>
+        <Text fontSize="1.2rem" lineHeight="1.5" color="#555">
+          {authorDescription}
+        </Text>
+      </Flex>
+    </Box>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,67 +1,19 @@
-import Image from "next/image";
-import AuthorCard from './AuthorCard';
-import CustomPagination from "./CustomPagination";
-import { useRouter } from 'next/router'; // Import the useRouter hook
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 2rem;
-  border-radius: 12px;
-  background-color: #fff;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  max-width: 1200px;
-  margin: 2rem auto; /* Horizontally center the component */
-  
-  @media (max-width: 768px) {
-    /* Adjust as needed for mobile layout */
-  }
-};`;
-
-const ImageWrapper = styled.div`
-  width: 100%;
-  padding: 1vh;
-  max-width: 500px;
-
-  > div {
-    position: unset !important;
-  }
-
-  .image {
-    object-fit: contain;
-    width: 100% !important;
-    position: relative !important;
-    height: unset !important;
-  }
-`;
-
-const ImageWrapperP = styled.p`
-  margin: 0;
-  padding: 10px;
-`;
-
-const VideoWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  max-width: 100%;
-  border: none;
-  margin-bottom: 20px;
-`;
-
-const Message = styled.p`
-  color: red;
-  font-weight: bold;
-`;
-
-const PaginationWrapper = styled.div`
-  margin: 20px;
-`;
+import {
+  Box,
+  Center,
+  Heading,
+  Text,
+  Image,
+  VStack,
+  Alert,
+  AlertIcon,
+  Flex,
+  Spacer
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import AuthorCard from './AuthorCard';
+import CustomPagination from './CustomPagination';
 
 export default function Post(props) {
   const { id, pageCount, lang } = props;
@@ -83,55 +35,74 @@ export default function Post(props) {
     }, {});
 
     return (
-      <Wrapper>
-        <h1>{decodedProps[0]}</h1>
-        <p>{decodedProps[1]}</p>
-        {decodedProps[3] !== '' ? (
-          <ImageWrapper>
-            <Image
-              src={decodedProps[3]}
-              alt="Post image"
-              className={'image'}
-              width="1000"
-              height="1000"
-
-            />
-          </ImageWrapper>
+      <Box
+        p="2rem"
+        borderRadius="12px"
+        backgroundColor="white"
+        boxShadow="0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)"
+        maxW="1200px"
+        margin="2rem auto"
+      >
+          <Flex justify="center" align="center" width="100%" maxW="100%" my="20px" flexDirection="column">
+            <Box textAlign="center" mb="2rem">
+              <Heading as="h1" size="xl">{decodedProps[0]}</Heading>
+            </Box>
+            <Text fontSize="20px">{decodedProps[1]}</Text>
+          </Flex>
+          {decodedProps[3] !== '' ? (
+            <Flex justify="center" align="center" width="100%" maxW="100%" my="20px" flexDirection="column">
+              <Image
+                src={decodedProps[3]}
+                alt="Post image"
+                objectFit="contain"
+                width="100%"
+                height="auto"
+                my="1vh"
+                maxW="500px"
+              />        
+            </Flex>
         ) : (
-          <ImageWrapper>
-            <Image
-              src={decodedProps[4]}
-              alt="No image"
-              className={'image'}
-              width="1000"
-              height="1000"
-            />
-            <ImageWrapperP>No image available</ImageWrapperP>
-          </ImageWrapper>
+            <Flex justify="center" align="center" width="100%" maxW="100%" my="20px" flexDirection="column">
+              <Image
+                src={decodedProps[4]}
+                alt="No image"
+                objectFit="contain"
+                width="100%"
+                height="auto"
+                my="1vh"
+                maxW="500px"
+              />
+              <Alert status="error" maxW="500px">
+              <AlertIcon />
+                No image available
+              </Alert>       
+            </Flex>
         )}
-        <br />
-        <VideoWrapper>
+        <Flex justify="center" align="center" width="100%" maxW="100%" my="20px" flexDirection="column">
           {decodedProps[2] !== '' ? (
             <iframe src={decodedProps[2]} width="640" height="352"></iframe>
           ) : (
-            <Message>No video available</Message>
+            <Alert status="error" maxW="500px">
+              <AlertIcon />
+              No video available
+            </Alert>
           )}
-        </VideoWrapper>
-
-        <AuthorCard
-          authorName={decodedProps[6]}
-          authorImg={decodedProps[8]}
-          authorDescription={decodedProps[11]}
-        />
-        <PaginationWrapper>
+        
+          <AuthorCard
+            authorName={decodedProps[6]}
+            authorImg={decodedProps[8]}
+            authorDescription={decodedProps[11]}
+          />
+        </Flex>
+        <Box margin="20px">
           <CustomPagination
             currentPage={parseInt(id)}
             totalPages={parseInt(pageCount)}
             onPageChange={handlePageChange}
             lang={lang}
           />
-        </PaginationWrapper>
-      </Wrapper>
+        </Box>
+      </Box>
     );
   } catch (error) {
     // Handle the error here
